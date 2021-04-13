1db8d10fcc789572bb4d9f3b46262910287e163f
Luc Léonard <github@luc-leonard.fr>
2021-04-13 12:27:41 +0200

    use cuda11 instead of cuda10

--- init_env.sh
+++ init_env.sh
@@ -1,2 +1,2 @@
 poetry update
-poetry run pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+poetry run pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
