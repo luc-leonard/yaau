7f596b8c5566a77c92fae037f3c3016040690e6e
Luc Léonard <github@luc-leonard.fr>
2021-04-15 18:30:39 +0200

    ugly gui

---
+++ yauu/gui.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfile
+from tkinter.ttk import Progressbar
+
+from super_res import Upscaler
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+        Button(self, text='choose file', command=self.choose_input).pack(side='right')
+        Button(self, text='choose model', command=self.choose_model).pack(side='right')
+
+        Button(self, text='run', command=self.run).pack(side='bottom')
+
+        self.progress_bar = Progressbar(self, length=100, mode='determinate')
+        self.progress_bar.pack(side='bottom')
+
+    def choose_input(self):
+        filepath = askopenfilename()
+        self.input_file = filepath
+
+    def choose_model(self):
+        filepath = askopenfilename()
+        self.model = filepath
+
+    def run(self):
+        upscaler = Upscaler(self.input_file, self.model, 2, 64, 2, 'cuda')
+        for i in range(len(upscaler)):
+            print(i)
+            upscaler.upscale_tile(i)
+            self.progress_bar['value'] = (i / len(upscaler)) * 100
+            self.update_idletasks()
+        upscaler.result.save('result_from_gui.png')
+
+
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
--- yauu/super_res.py
+++ yauu/super_res.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from pathlib import Path
 from typing import List
 
 import PIL.Image
@@ -21,18 +22,20 @@ def grid(image: PIL.Image.Image, grid_size, overlap) -> List[List[PIL.Image.Imag
     return the_grid
 
 
-def main(args):
-    overlap = args.grid_size // 2
-    big_grid_size = (args.grid_size + overlap) * args.scale
+def super_res(file_path: Path, scale: int, grid_size: int, overlap_factor, model: Path, device: str) -> PIL.Image:
+    overlap = grid_size // overlap_factor
 
-    data = torch.load(args.model, map_location=args.device)
-    model = data['model'].to(args.device).eval()
+    big_grid_size = (grid_size + overlap) * scale
 
-    image = PILImage.create(sys.argv[-1])
+    data = torch.load(model, map_location=device)
+    model = data['model'].to(device).eval()
 
-    the_grid = grid(image, args.grid_size, overlap)
-    full_w = (big_grid_size - (overlap * args.scale)) * len(the_grid[0])
-    full_h = (big_grid_size - (overlap * args.scale)) * len(the_grid)
+    image = PILImage.create(file_path)
+
+    the_grid = grid(image, grid_size, overlap)
+    full_w = (big_grid_size - (overlap * scale)) * len(the_grid[0])
+    full_h = (big_grid_size - (overlap * scale)) * len(the_grid)
+
     result = PIL.Image.new('RGBA', (full_h, full_w))
 
     alpha_circle = PIL.Image.new('1', (big_grid_size, big_grid_size))
@@ -40,17 +43,76 @@ def main(args):
 
     for x, row in enumerate(the_grid):
         for y, element in enumerate(row):
-            tensor = ToTensor()(element.resize((big_grid_size, big_grid_size))).unsqueeze(0).float().to(args.device)
+            tensor = ToTensor()(element.resize((big_grid_size, big_grid_size))).unsqueeze(0).float().to(device)
             img_hr, *_ = model(tensor)
             img_hr = to_image(img_hr.clip(0, 1))
             result.paste(img_hr,
                          (
-                             x * (big_grid_size - (overlap * args.scale)),
-                             y * (big_grid_size - (overlap * args.scale))
-                         ), alpha_circle)
+                             x * (big_grid_size - (overlap * scale)),
+                             y * (big_grid_size - (overlap * scale))
+                         ), alpha_circle
+                         )
             del tensor
             del img_hr
-    result.save(args.output)
+    return result
+
+
+class Upscaler:
+    def __init__(self, image_path: Path, model, scale: int, grid_size: int, overlap_factor: int, device: str):
+        # the image to upscale
+        self.image = PILImage.create(image_path)
+        # the model to use
+        data = torch.load(model, map_location=device)
+        self.model = data['model'].to(device).eval()
+        # the scaling factor
+        self.scale = scale
+        # the base size of each tile
+        self.grid_size = grid_size
+        # how much to overlap between tiles ?
+        self.overlap = self.grid_size // overlap_factor
+        # cpu or cuda
+        self.device = device
+        self.tiles = self._grid()
+        # the size of each upscaled tile
+        self.big_grid_size = (grid_size + self.overlap) * scale
+
+        self.alpha_circle = PIL.Image.new('1', (self.big_grid_size, self.big_grid_size))
+        ImageDraw.Draw(self.alpha_circle).ellipse([(0, 0), (self.big_grid_size, self.big_grid_size)], fill=(255,))
+
+        full_w = (self.big_grid_size - (self.overlap * self.scale)) * len(self.tiles[0])
+        full_h = (self.big_grid_size - (self.overlap * self.scale)) * len(self.tiles)
+        self.result = PIL.Image.new('RGBA', (full_h, full_w))
+
+    def _grid(self) -> List[List[PIL.Image.Image]]:
+        return grid(self.image, self.grid_size, self.overlap)
+
+    def __len__(self):
+        return len(self.tiles) * len(self.tiles[0])
+
+    def upscale_tile(self, idx):
+        x_idx = idx % len(self.tiles[0])
+        y_idx = idx // len(self.tiles[0])
+        tile_to_upscale = self.tiles[y_idx][x_idx]
+        tile_to_upscale.resize((self.big_grid_size, self.big_grid_size))
+        tensor = ToTensor()(tile_to_upscale.resize((self.big_grid_size, self.big_grid_size))).unsqueeze(0).float().to(
+            self.device)
+        img_hr, *_ = self.model(tensor)
+        img_hr = to_image(img_hr.clip(0, 1))
+        self.result.paste(img_hr,
+                          (
+                              y_idx * (self.big_grid_size - (self.overlap * self.scale)),
+                              x_idx * (self.big_grid_size - (self.overlap * self.scale))
+                          ), self.alpha_circle
+                          )
+
+
+def main(args):
+    upscaler = Upscaler(sys.argv[-1], args.model, args.scale, args.grid_size, 2, args.device)
+    for i in range(len(upscaler)):
+        print(f'upscaling {i} / {len(upscaler)}')
+        upscaler.upscale_tile(i)
+
+    upscaler.result.save(args.output)
 
 
 def get_arguments():
